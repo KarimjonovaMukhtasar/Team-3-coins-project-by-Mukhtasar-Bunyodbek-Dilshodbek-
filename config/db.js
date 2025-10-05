b52872026fd0099b056cfbf8c9e62beb04a1b5f2
Bunyodbek G'ulomjonov <bunyodbek@Bunyodbeks-MacBook-Air.local>
2025-10-05 13:03:32 +0500

    Users comments collections tugadi

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 import pg from "pg"
 const { Pool } = pg
-const pool = new Pool()
-
-pool.use({
+const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    password: "root",
     database: "coins",
     port: 5432
 })
--- routes/collections.router.js
+++ routes/collections.router.js
@@ -1,1 +1,13 @@
 // BUNYODBEK
+import{Router} from "express";
+import {findAll, findOne, createOne, updateOne, deleteOne, search} from "../controllers/collections.controller.js"
+const router = Router()
+
+router.get("/search", search)
+router.get("/", findAll);
+router.get("/:id", findOne);
+router.post("/", createOne);
+router.put("/:id",updateOne )
+router.delete("/:id",deleteOne)
+
+export default router 
